278ba642b5b7c34f66197cf0304a706a62926dde
JuanGPC <juangpc@users.noreply.github.com>
2023-02-01 14:25:18 -0500

    create calls lib

---
+++ libs/calls/calls.hpp
@@ -0,0 +1,14 @@
+#ifndef LIBS_CALLS_CALLS_HPP_
+#define LIBS_CALLS_CALLS_HPP_
+
+namespace calls {
+
+void callback1(int a);
+
+void callback2(int a);
+
+void callback3(int a);
+
+}
+
+#endif  //lLIBS_CALLS_CALLS_HPP_
