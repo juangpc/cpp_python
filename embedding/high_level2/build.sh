48da5c2becb6739de6ddac0192295313dd7611c4
JuanGPC <juangpc@users.noreply.github.com>
2023-02-03 21:10:50 -0500

    highlevel2 working

--- embedding/high_level2/build.sh
+++ embedding/high_level2/build.sh
@@ -1,11 +1,32 @@
 #!/bin/sh
 #
 
-rm -fr build
-rm call
-cmake -B build -S .
+SCRIPT_PATH="$0"
+ROOT_FOLDER=""
+ROOT_FOLDER="$(cd -- "$(dirname $SCRIPT_PATH)" && pwd)"
+
+BUILD_FOLDER=$ROOT_FOLDER/build
+SRC_FOLDER=$ROOT_FOLDER
+
+echo ""
+echo BUILD_FOLDER=$BUILD_FOLDER
+
+function deleteBuildFolder() {
+  rm -fr $BUILD_FOLDER
+  rm call
+}
+
+if [[ $1 == "clean"  ]]; then
+  deleteBuildFolder
+  cmake -B build -S .
+fi
+
 cmake --build build
 
+# call application
+echo ""
+echo "Executing:"
+echo "----------"
+
 ./call hello say_hello1
-echo " "
 ./call hello say_hello2
--- embedding/high_level2/main.c
+++ embedding/high_level2/main.c
@@ -2,9 +2,6 @@
 #include <Python.h>
 
 int main(int argc, char *argv[]) {
-  
-  printf("Hello!\n");
-
   PyObject *pName = NULL;
   PyObject *pModule = NULL;
   PyObject *pFunc = NULL;
@@ -18,11 +15,29 @@ int main(int argc, char *argv[]) {
   PyWideStringList_Append(&config.module_search_paths, L".");
   Py_InitializeFromConfig(&config);
 
+  if (argc < 3) {
+    printf("Usage: %s module_name function_name.\n", argv[0]);
+    exit(1);
+  }
+
   pName = PyUnicode_FromString(argv[1]);
+  if (pName == NULL) {
+    printf("Problem parsing the module name.\n");
+    exit(1);
+  }
   pModule = PyImport_Import(pName);
   Py_XDECREF(pName);
+  if (pModule == NULL) {
+    printf("Problem importing the module.\n");
+    exit(1);
+  }
 
   pFunc = PyObject_GetAttrString(pModule, argv[2]);
+  if (pFunc == NULL) {
+    printf("Function %s in module %s could not be found.\n", argv[2], argv[1]);
+    exit(1);
+  }
+
   pValue = PyObject_CallNoArgs(pFunc);  // actual call to the fcn
 
   Py_XDECREF(pValue);
@@ -30,5 +45,6 @@ int main(int argc, char *argv[]) {
   Py_XDECREF(pModule);
 
   Py_Finalize();
+
   return 0;
 }
